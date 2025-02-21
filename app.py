b8da957924ea762606459e076b367747b88521c7
SulmanAwan <113705370+SulmanAwan@users.noreply.github.com>
2025-02-21 21:33:15 +0000

    added the Flask project

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__) #importing a class and creating an instance of the class
+
+@app.route('/') #maps to the homepage of jovian.com, when the url / is accessed run hello_world.
+def hello_world():
+  return "Hello, Jovian"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True) #Host 0.0.0.0 means it will be run from the local machine.
