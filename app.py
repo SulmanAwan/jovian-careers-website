8422ac50b871a190f066499d9a660faaed4f76c8
SulmanAwan <113705370+SulmanAwan@users.noreply.github.com>
2025-02-21 22:31:27 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) #importing a class and creating an instance of the class
 
 @app.route('/') #maps to the homepage of jovian.com, when the url / is accessed run hello_world.
 def hello_world():
-  return "Hello, Jovian"
+  return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True) #Host 0.0.0.0 means it will be run from the local machine.
